39fbe42f9aecc01be8fe0a17a3e19645645cc69e
johney.song <joy1305@gmail.com>
2017-03-25 10:55:17 +0800

    remember me
    1.add rememb.er me function
    2.format code

--- app/lightgallery/js/lg-label.js
+++ app/lightgallery/js/lg-label.js
@@ -1,40 +1,46 @@
 import fs from 'fs';
+import { remote } from 'electron';
 (function ($, window, document, undefined) {
 
     'use strict';
 
     var curUser = "";
-    var users = ["sunwei"];
+    var users = ["sunwei","test"];
     var userPassMap = {};
-    
-    var tucaoUncertain = ["好担心好害怕","你给我小心点！"];
-    var tucaoYes = ["原来我没有病","你确定吗？我妈都说我有问题","原来我很正常啊，我一直以为自己有病呢!","太好了，给你一个赞！"];
-    var tucaoNo = ["你才抑郁，你们XX都抑郁","你确定你自己没问题吗?","我完了,我要抑郁了!","我要找个地方去上吊!","我要去医闹!","我很桑心"];
-    var tucaoParts = ["就你长得好看!","我知道了，你就是看我不顺眼","别挑我毛病了，看看你自己就长得好吗？"];
-    var initUserAccount = function (){
+
+    var tucaoUncertain = ["好担心好害怕", "你给我小心点！"];
+    var tucaoYes = ["原来我没有病", "你确定吗？我妈都说我有问题", "原来我很正常啊，我一直以为自己有病呢!", "太好了，给你一个赞！"];
+    var tucaoNo = ["你才抑郁，你们XX都抑郁", "你确定你自己没问题吗?", "我完了,我要抑郁了!", "我要找个地方去上吊!", "我要去医闹!", "我很桑心"];
+    var tucaoParts = ["就你长得好看!", "我知道了，你就是看我不顺眼", "别挑我毛病了，看看你自己就长得好吗？"];
+    var initUserAccount = function () {
         userPassMap["sunwei"] = "1q2w3e4r";
+        userPassMap["test"] = "1";
     }
 
-    function randomNum(minNum,maxNum){ 
-        switch(arguments.length){ 
-            case 1: 
-                return parseInt(Math.random()*minNum+1); 
-                break; 
-            case 2: 
-                return parseInt(Math.random()*(maxNum-minNum+1)+minNum); 
-                break; 
-            default: 
-                return 0; 
-                break; 
-    } 
-} 
-    
+    function randomNum(minNum, maxNum) {
+        switch (arguments.length) {
+            case 1:
+                return parseInt(Math.random() * minNum + 1);
+                break;
+            case 2:
+                return parseInt(Math.random() * (maxNum - minNum + 1) + minNum);
+                break;
+            default:
+                return 0;
+                break;
+        }
+    }
+
     var defaults = {
         label: true,
-        tucaoMode: true
+        tucaoMode: true,
     };
 
-    function newLabel(){
+    var userData = {
+        user: ""
+    }
+
+    function newLabel() {
         return {
             yes: false,
             no: false,
@@ -94,7 +100,7 @@ import fs from 'fs';
             $labelyes.removeClass('lg-label-yesno-selected');
             $labelno.addClass('lg-label-yesno-selected');
             $labelparts.css('visibility', 'visible');
-        }else{
+        } else {
             $labelno.removeClass('lg-label-yesno-selected');
         }
         if (curLabel.forehead) {
@@ -121,7 +127,7 @@ import fs from 'fs';
     }
 
     Label.prototype.updateTuCao = function (caoStrs) {
-        if(this.core.s.tucaoMode){
+        if (this.core.s.tucaoMode) {
             var index = randomNum(0, caoStrs.length - 1);
             var caoStr = caoStrs[index];
             var $labelBar = this.core.$outer.find('.lg-labelbar');
@@ -134,14 +140,14 @@ import fs from 'fs';
         var _this = this;
 
         initUserAccount();
-
+        loadUser();
         _this.core.$el.on('onBeforeSlide.lg', function (el, _prevIndex, index, fromTouch, fromThumb, src) {
             console.log("onBeforeSlide.lg; index:" + index + "; src:" + src);
             var pos = src.lastIndexOf('.');
             var filePathName = src.slice(0, pos);
             pos = filePathName.lastIndexOf('/');
-            fileName = filePathName.slice(pos+1);
-            filePath = filePathName.slice(0, pos+1);
+            fileName = filePathName.slice(pos + 1);
+            filePath = filePathName.slice(0, pos + 1);
             updateFileAll();
             fs.readFile(fileAll, function (err, data) {
                 if (err) {
@@ -158,7 +164,7 @@ import fs from 'fs';
         var userName = '<div class="lg-labels-user-group">\
                             <span class = "lg-label-text">医生：</span>\
                             <br/>\
-                            <span id="lg-label-user" class="lg-label-user">'+curUser+'</span>\
+                            <span id="lg-label-user" class="lg-label-user">'+ curUser + '</span>\
                         </div>';
         var labelTucao = '<div class="lg-labels-tucao-group">\
                             <span class="lg-labels-tucao-text"></span>\
@@ -187,7 +193,7 @@ import fs from 'fs';
         });
         $labelyes.on('click.lg', function () {
             console.log("label yes");
-            if(checkAccount(false) === false){
+            if (checkAccount(false) === false) {
                 return;
             }
             curLabel.yes = true;
@@ -200,7 +206,7 @@ import fs from 'fs';
         });
         $labelno.on('click.lg', function () {
             console.log("label no");
-            if(checkAccount(false) === false){
+            if (checkAccount(false) === false) {
                 return;
             }
             curLabel.yes = false;
@@ -219,7 +225,7 @@ import fs from 'fs';
 
         $labelforehead.on('click.lg', function () {
             console.log("label forehead");
-            if(checkAccount(false) === false){
+            if (checkAccount(false) === false) {
                 return;
             }
             curLabel.forehead = !curLabel.forehead;
@@ -233,7 +239,7 @@ import fs from 'fs';
         });
         $labeleyes.on('click.lg', function () {
             console.log("label eyes");
-            if(checkAccount(false) === false){
+            if (checkAccount(false) === false) {
                 return;
             }
             curLabel.eyes = !curLabel.eyes;
@@ -247,7 +253,7 @@ import fs from 'fs';
         });
         $labelmouth.on('click.lg', function () {
             console.log("label mouth");
-            if(checkAccount(false) === false){
+            if (checkAccount(false) === false) {
                 return;
             }
             curLabel.mouth = !curLabel.mouth;
@@ -261,7 +267,7 @@ import fs from 'fs';
         });
         $labelcheek.on('click.lg', function () {
             console.log("label cheek");
-            if(checkAccount(false) === false){
+            if (checkAccount(false) === false) {
                 return;
             }
             curLabel.cheek = !curLabel.cheek;
@@ -274,7 +280,7 @@ import fs from 'fs';
             _this.updateTuCao(tucaoParts);
         });
 
-        
+
     };
 
     Label.prototype.destroy = function () {
@@ -283,8 +289,8 @@ import fs from 'fs';
 
     $.fn.lightGallery.modules.label = Label;
 
-    var checkAccount = function (force){
-        if(curUser !== "" && force === false){
+    var checkAccount = function (force) {
+        if (curUser !== "" && force === false) {
             return true;
         }
         var modal = document.getElementById('login');
@@ -295,7 +301,7 @@ import fs from 'fs';
         return false;
     }
 
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         var modal = document.getElementById('login');
         if (event.target === modal) {
             modal.style.display = "none";
@@ -304,20 +310,20 @@ import fs from 'fs';
         if (event.target === submit) {
             var user = document.getElementById('login-user').value;
             var pass = document.getElementById('login-pass').value;
-            if(users.indexOf(user)===-1){
+            if (users.indexOf(user) === -1) {
                 console.log("user not found" + user);
                 var x = document.getElementById("toast")
                 x.className = "show";
                 x.innerText = "User not found！";
-                setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+                setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
                 return;
             }
-            if(userPassMap[user] != pass){
+            if (userPassMap[user] != pass) {
                 console.log("increated password");
                 var x = document.getElementById("toast")
                 x.className = "show";
                 x.innerText = "Increated password！";
-                setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+                setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
                 return;
             }
             curUser = user;
@@ -326,9 +332,38 @@ import fs from 'fs';
             modal.style.display = "none";
             var userDom = document.getElementById('lg-label-user');
             userDom.innerText = user;
+            if (document.getElementById('login-rememberme').checked === true){
+                saveUser();
+            }else{
+                clearUser();
+            }
+            
         }
     }
 
-    
+    var app = remote.app;
+    var saveUser = function() {
+        userData.user = curUser;
+        fs.writeFile(app.getPath('userData') + '/lg-user.json', JSON.stringify(userData), function(err) {
+            if (err) throw err;
+        });
+    }
+
+    var clearUser = function() {
+        userData.user = "";
+        fs.writeFile(app.getPath('userData') + '/lg-user.json', JSON.stringify(userData), function(err) {
+            if (err) throw err;
+        });
+    }
+
+    var loadUser = function(){
+        fs.readFile(app.getPath('userData') + '/lg-user.json', function(err, data) {
+            if (err) throw err;
+            userData = JSON.parse(data);
+            curUser = userData.user;
+            var userDom = document.getElementById('lg-label-user');
+            userDom.innerText = curUser;
+        });
+    }
 
 })(jQuery, window, document);
